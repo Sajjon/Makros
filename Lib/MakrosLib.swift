0f8c0f1bf946b01f3383d9ffa99a234710221f33
Alexander Cyon <alex.cyon@gmail.com>
2023-06-10 19:15:14 +0200

    WIP

--- Lib/MakrosLib.swift
+++ Lib/MakrosLib.swift
@@ -1,7 +1,7 @@
 import Foundation
 import MakrosPlugin
 
-@attached(member, names: named(init(data:)), arbitrary)
+@attached(member, names: named(init(data:)))
 public macro DataStorage(named: String = DataStorageMacro.defaultStorageName) = #externalMacro(
 	module: "MakrosPlugin",
 	type: "DataStorageMacro"
